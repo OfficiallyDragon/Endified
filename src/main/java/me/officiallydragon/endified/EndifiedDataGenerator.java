8a7c1424c40154468229e0220abb9603ef18dcbd
OfficiallyDragon <dexter@dexterwheatcroft.uk>
2024-04-19 10:37:20 +0100

    Initial commit

---
+++ src/main/java/me/officiallydragon/endified/EndifiedDataGenerator.java
@@ -0,0 +1,11 @@
+package me.officiallydragon.endified;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EndifiedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
